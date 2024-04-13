4e14ce1990e611958226d352a70410bff7554775
En-Geon <58935802+En-Geon@users.noreply.github.com>
2024-04-13 21:34:00 +0900

    login 04-13v1

---
+++ site/module/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.shortcuts import render
+from .models import Post
+
+
+def index(request):
+    post_list = Post.objects.order_by('-create_date')
+    context = {'post_list': post_list}
+    return render(request, 'module/index.html', context)
