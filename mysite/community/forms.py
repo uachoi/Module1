07f041de5ec7d4d81a9f00afcd92781d59170592
junggju <166771145+junggju@users.noreply.github.com>
2024-04-14 18:58:01 +0900

    Add files via upload

---
+++ mysite/community/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+from .models import Post
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['title', 'content', 'image1', 'image2', 'image3']
+
+
+class SearchForm(forms.Form):
+    query = forms.CharField(label='Search', max_length=100)
+
---
+++ mysite/community/urls.py
@@ -0,0 +1,27 @@
+from django.urls import path
+from django.contrib.auth import views as auth_views
+from . import views
+from community import views
+
+
+
+
+
+
+urlpatterns = [
+    path('', views.index),
+    path('login.html', views.login_page, name='login'),
+    path('my_page.html', views.my_page, name='my_page'),
+    path('main.html', views.main, name='main'),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('loginok.html', views.loginok_page, name='loginok'),
+    path('logout.html/', auth_views.LogoutView.as_view(), name='logout'),
+    path('my_page2.html/', views.my_page2, name='my_page2'),
+    path('community/create_post/', views.create_post, name='create_post'),
+  
+
+   
+
+      
+
+]
